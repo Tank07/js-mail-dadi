a08a20330fc26b6fcd83455b1cd09e817576e8ce
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-15 16:24:25 +0100

    Prime prove

--- coda/main.js
+++ coda/main.js
@@ -7,12 +7,6 @@
 // Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri interi casuali quanti sono gli elementi da inserire.
 // Attenzione #3: quali controlli dobbiamo fare sull'input dell'utente?
 // Super Extra Bonus: chiedere all'utente anche quanti elementi vanno estratti dal fondo dell'array.
-// Consigli del giorno:
-// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-// 3. si ma noi cosa vogliamo fare?
-// 4. torniamo a scrivere in italiano
-// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
 
 // Bonus 1
@@ -24,3 +18,11 @@ for ( i = (arrayIo.length - 1); i>3; i-- ){
     document.getElementById("ultimi_cinque").innerHTML += `<div>${arrayIo[i]}</div>`;
     console.log(arrayIo[i])
   }
+
+//   Extra Bonus
+
+let elementi = prompt("Quanti elementi dovrà contenere l'array ?");
+let arrayPrompt = [];
+arrayPrompt.lenght = elementi;
+console.log(arrayPrompt)
+
