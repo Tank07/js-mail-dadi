44603296ee9de776baaa74a54a4fb2df42481e5a
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-15 14:04:20 +0100

    Ancora capisco errore

--- mail/main.js
+++ mail/main.js
@@ -4,12 +4,18 @@
     // (non usare INCLUDES)    
 
     let email = prompt("Qual è la tua mail ?");
-    console.log(email)
-    let accesso = ["banana@hotmail","jabroni@outlook", "tanc@hotmail" ];
-    console.log(accesso)
+    console.log(`email = ${email}`)
+    let arrayAccesso = ["banana@hotmail","jabroni@outlook", "tanc@hotmail"];
+    console.log(arrayAccesso)
 
-    if (email == accesso){
-        document.getElementById("msg_appropriato").innerHTML = `<div class= "green">Accesso consentito tramite mail: <span class= "blue">${email}</span>  al server Boolean.</div>`;
-    } else{
-        document.getElementById("msg_appropriato").innerHTML = `<div class="red">Accesso non consentito tramite mail: <span class= "blue">${email}</span> al server Boolean, la SWAT è in viaggio verso casa sua.</div>`;
-    }
+    for( i=0; i < arrayAccesso.length; i++ ){
+
+        if ( email == arrayAccesso[i] ){
+            console.log(email , arrayAccesso[i])
+            document.getElementById("msg_appropriato").innerHTML = `<div class= "green">Accesso consentito tramite mail: <span class= "blue">"${email}"</span>  al server Boolean.</div>`;
+        } else  {
+            console.log(email , arrayAccesso[i])
+            document.getElementById("msg_appropriato").innerHTML = `<div class="red">Accesso non consentito tramite mail: <span class= "blue">"${email}"</span> al server Boolean, la SWAT è in viaggio verso casa sua.</div>`;          
+        }
+      
+      }
