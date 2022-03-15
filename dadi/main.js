ba3d81e63c5ab452ce8d5c0eefac728a6bebc2b0
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-15 15:44:30 +0100

    Fatto esercizio dadi senza array

--- dadi/main.js
+++ dadi/main.js
@@ -3,3 +3,19 @@
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+let numeroUtente = Math.floor(Math.random() * 6) + 1;
+console.log( `Il numero del giocatore è = ${numeroUtente}`);
+let numeroComputer = Math.floor(Math.random() * 6) + 1;
+console.log( `Il numero del computer è = ${numeroComputer}`);
+
+if (numeroUtente > numeroComputer) {
+    console.log(numeroUtente, numeroComputer);
+    document.getElementById("esito").innerHTML = `<div>Vince il giocatore con il numero <span>"${numeroUtente}"</span> , essendo maggiore del numero del computer <span>"${numeroComputer}"</span> </div>`;
+} else if (numeroUtente < numeroComputer) {
+    console.log(numeroUtente, numeroComputer);
+    document.getElementById("esito").innerHTML = `<div>Vince il computer con il numero <span>"${numeroComputer}"</span> , essendo maggiore del numero del giocatore <span>"${numeroUtente}"</span> </div>`;
+}  else if (numeroUtente == numeroComputer) {
+        console.log(numeroUtente, numeroComputer);
+        document.getElementById("esito").innerHTML = `<div>Si è verificato un pareggio essendo <span>"${numeroUtente}"</span> , e <span>"${numeroComputer}"</span> lo stesso numero </div>`;
+}
